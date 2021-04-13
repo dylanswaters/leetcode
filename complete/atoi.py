162c71b58af26ec417c4f7675425c2e484948f17
Dylan Waters <dylanswaters@gmail.com>
2021-04-13 10:59:30 -0600

    Add files via upload

---
+++ complete/atoi.py
@@ -0,0 +1,22 @@
+# leetcode 8
+
+def myAtoi(a):
+    fStr = a.strip()
+    space = fStr.find(' ')
+    # print(space)
+    if(space != -1):
+        fStr = fStr[0:fStr.find(' ')]
+    print("strip: " + fStr)
+    if(fStr.isdigit()):
+        print("here")
+        return int(fStr)
+
+def main():
+    print(myAtoi("42"))
+    print(myAtoi("   -42"))
+    print(myAtoi("4193 with words"))
+    print(myAtoi("words and 987"))
+    print(myAtoi("-91283472332"))
+
+if __name__ == "__main__":
+    main()
